d81bd13d1092c4b28479a22f1fad0dad6ace9fae
Chanala Wilangka <c.wilangka@gmail.com>
2016-08-24 15:16:36 +0700

    inits getkickstart [+] init navbar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-   
-
-/*
-   $(function() {
-   
-	});
-*/
 
 
 });
+
+function openNavList() {
+    var navList = document.querySelector('.navList')
+    navList.classList.toggle('collapse');
+}
