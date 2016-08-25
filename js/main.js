85ba5ee147b669b77c80abd684d4bc538636ef2b
Chanala Wilangka <c.wilangka@gmail.com>
2016-08-25 13:03:38 +0700

    Progess section_triple [+] carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-
+    $('.owl-carousel.loop').owlCarousel({
+        center: true,
+        items: 1,
+        loop: true,
+        margin: 10,
+        responsive: {
+            600: {
+                items: 2
+            }
+        }
+    });
 
 });
 
