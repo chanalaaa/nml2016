7e399a207e56272380159dfb745d09403cacaac4
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-01 14:44:50 +0700

    detailed .bgTypo.m (.home)

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
 
         var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
         new ScrollMagic.Scene({triggerElement: "#parallax1"})
-                    .setTween(".bgTypo", {y: "50%", ease: Linear.easeNone})
+                    .setTween(".bgTypo", {y: "60%", ease: Linear.easeNone})
                     .addTo(controller);
 
 });
